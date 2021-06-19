dcba7247b6a35ecdb1c36d76d6b1d306857051d1
Froyo-Swap <froyoswap@gmail.com>
2021-06-19 12:08:07 +0100

    initial commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,156 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://dex.froyoswap.com",
+      },
+      {
+        label: "Liquidity",
+        href: "https://dex.froyoswap.com/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://froyoswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://froyoswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://froyoswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://froyoswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Voting",
+        href: "https://voting.froyoswap.com",
+      },
+      {
+        label: "Github",
+        href: "https://github.com/pancakeswap",
+      },
+      {
+        label: "Docs",
+        href: "https://docs.froyoswap.com",
+      },
+      {
+        label: "Blog",
+        href: "https://froyoswap.medium.com",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Github",
+    icon: "GithubIcon",
+    href: "https://github.com/froyoswap",
+  },
+  {
+    label: "Docs",
+    icon: "GitbookIcon",
+    href: "https://docs.froyoswap.com",
+  },
+  {
+    label: "Medium",
+    icon: "MediumIcon",
+    href: "https://froyoswap.medium.com",
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/FroyoSwap",
+  },
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/FroyoSwap",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/FroyoSwapAnn",
+      },
+    ],
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
